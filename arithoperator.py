c7868c8e205a0d83576ae63ad294bbed40767124
obinonny <obioranonyelu@yahoo.com>
2019-05-14 17:44:23 +0200

    https://www.hackerrank.com/challenges/python-arithmetic-operators/problem

---
+++ arithoperator.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+add=a+b
+sub= a-b
+mult=a*b
+print(add)
+print(sub)
+print(mult)
+
+#https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
