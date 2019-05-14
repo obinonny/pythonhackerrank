61aa3f2b6267a4e224967dfbc1e85dfed46cd99a
obinonny <obioranonyelu@yahoo.com>
2019-05-14 17:27:19 +0200

    https://www.hackerrank.com/challenges/python-division/problem

---
+++ pythondivsion.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+division_int=a//b
+division_float=a/b
+print(division_int)
+print(division_float)
+
+#https://www.hackerrank.com/challenges/python-division/problem
